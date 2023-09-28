52e13991ce2dc9f98009cc29eaa421c72ce2aabc
ErnestYamboar <olughcom@gmail.com>
2023-09-27 22:25:04 -0700

    betty not done yet

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,16 +4,10 @@
 
 
 /**
-
  * print_list - prints all the elements of a linked list
-
  * @h: pointer to the list_t list to print
-
- *
-
  * Return: the number of nodes printed
-
-*/
+ */
 
 size_t print_list(const list_t *h)
 
